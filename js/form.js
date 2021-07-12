940a8007c99e935b65b7fd406d1aa1cf6b46d4d4
Alejandro Báez <baezdalex@gmail.com>
2021-07-12 12:28:11 -0500

    ✨ Feat: add bootstrap

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 const form = document.getElementById("form-checkout");
 const submitButton = document.getElementById("submit-btn");
-
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const fullName = document.getElementById("fullName");
@@ -8,11 +7,6 @@ const address = document.getElementById("address");
 const city = document.getElementById("city");
 const country = document.getElementById("selectCountry");
 const postalCode = document.getElementById("postalCode");
-const selectCountries = new vanillaSelectBox("#selectCountry", {
-  placeHolder: "Your Country",
-  maxSelect: 3,
-  translations: { all: "All", items: "Countries" },
-});
 
 const btnIncrement = document.getElementById("incrementVintageBackpack");
 const btnDecrement = document.getElementById("decrementVintageBackpack");
@@ -28,117 +22,6 @@ const offerVintageBackpack = parseFloat(
 const offerLeviShoes = parseFloat(
   document.getElementById("offerLeviShoes").innerText.substring(1)
 );
-
-const ERROR_MESSAGES = {
-  BLANK: (ref, error) => showError(ref, error),
-  EMAIL: (ref, error) => {
-    if (!isEmail(ref.value)) {
-      setErrorFor(ref, error);
-    } else {
-      setSuccessFor(ref);
-    }
-  },
-  PHONE: (ref, error) => {
-    if (!isPhoneNumber(ref.value)) {
-      setErrorFor(ref, error);
-    } else {
-      setSuccessFor(ref);
-    }
-  },
-  SELECT: (ref, error) => {
-    if (getValuesFromSelect("selectCountry").length === 0) {
-      setErrorFor(ref, error);
-    } else {
-      setSuccessForSelect(ref);
-    }
-  },
-};
-const getDataWithErrorMessages = () => {
-  return [
-    {
-      id: 1,
-      ref: email,
-      message: "Email cannot be blank",
-      type: "BLANK",
-    },
-    {
-      id: 12,
-      ref: email,
-      message: "Not a valid email",
-      type: "EMAIL",
-    },
-    {
-      id: 2,
-      ref: fullName,
-      message: "Full name cannot be blank",
-      type: "BLANK",
-    },
-    {
-      id: 3,
-      ref: phone,
-      message: "Phone cannot be blank",
-      type: "PHONE",
-    },
-    {
-      id: 4,
-      ref: address,
-      message: "Address cannot be blank",
-      type: "BLANK",
-    },
-    {
-      id: 5,
-      ref: city,
-      message: "City cannot be blank",
-      type: "BLANK",
-    },
-    {
-      id: 6,
-      ref: postalCode,
-      message: "Postal cannot be blank",
-      type: "BLANK",
-    },
-    {
-      id: 7,
-      ref: country,
-      message: "Choose a country",
-      type: "SELECT",
-    },
-  ];
-};
-
-const validate = () => {
-  const data = getDataWithErrorMessages();
-  data.forEach((element) => {
-    if (ERROR_MESSAGES[element.type]) {
-      ERROR_MESSAGES[element.type](element.ref, element.message);
-    }
-  });
-};
-
-const showError = (element, message) => {
-  if (isEmpty(element)) setErrorFor(element, message);
-  else setSuccessFor(element);
-};
-
-const setErrorFor = (input, message) => {
-  const formControl = input.closest(".form-control");
-  const small = formControl.querySelector("small");
-
-  formControl.className = "form-control error";
-  small.innerText = message;
-};
-
-const setSuccessFor = (input) => {
-  const formControl = input.closest(".form-control");
-  formControl.className = "form-control success";
-};
-
-const setSuccessForSelect = () => {
-  const select = document.getElementById("btn-group-selectCountry");
-  select.className = "vsb-main success";
-};
-
-const isEmpty = (element) => (element.value.trim() === "" ? true : false);
 
 const getMessage = () =>
   "===== Ticket =======" +
@@ -165,14 +48,6 @@ const getMessage = () =>
   totalAmount.innerText +
   "\n=================";
 
-const removeStylesSuccess = () => {
-  getDataWithErrorMessages().forEach((element) => {
-    const formControl = element.ref.closest(".form-control");
-    formControl.className = "form-control";
-  });
-  const select = document.getElementById("btn-group-selectCountry");
-  select.className = "vsb-main";
-};
 const getValuesFromSelect = (id) => {
   let result = [];
   let collection = document.querySelectorAll("#" + id + " option");
@@ -183,37 +58,42 @@ const getValuesFromSelect = (id) => {
   });
   return result;
 };
-const isEmail = (email) => {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
+
+(function () {
+  "use strict";
+
+  window.addEventListener(
+    "load",
+    function () {
+      let forms = document.getElementsByClassName("needs-validation");
+
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              form.classList.add("was-validated");
+              event.preventDefault();
+              event.stopPropagation();
+            }
+
+            if (form.checkValidity() === true) {
+              event.preventDefault();
+              event.stopPropagation();
+              alert(getMessage());
+              form.classList.remove("was-validated");
+              form.reset();
+              // bootstrap-select
+              $(".selectpicker").selectpicker("deselectAll");
+            }
+          },
+          false
+        );
+      });
+    },
+    false
   );
-};
-
-const isPhoneNumber = (phone) => {
-  return /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(phone);
-};
-
-submitButton.addEventListener("click", validate);
-
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("👀 Please email address");
-  } else {
-    email.setCustomValidity("");
-    setSuccessFor(email);
-  }
-});
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  if (getValuesFromSelect("selectCountry").length > 0) {
-    alert(getMessage());
-    form.reset();
-    selectCountries.empty();
-    removeStylesSuccess();
-  }
-});
+})();
 
 /*=====================Products================================== */
 btnIncrement.addEventListener("click", () =>
